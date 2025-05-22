18d4d6e358f1d9431ccc39ee0c40e6c72a604a29
Asrarul Hoque Eusha <asrarul.hoque@brainstation-23.com>
2025-05-22 19:38:11 +0600

    Add DTO models for cart

---
+++ DainikBazar.Application/Common/DTOs/CartItemDto.cs
@@ -0,0 +1,13 @@
+﻿
+
+namespace DainikBazar.Application.Common.DTOs;
+
+public class CartItemDto
+{
+    public int Id { get; set; }
+    public int Quantity { get; set; }
+    public decimal UnitPrice { get; set; }
+    public int ProductId { get; set; }
+    public string ProductName { get; set; }
+    public int CartId { get; set; }
+}
---
+++ DainikBazar.Application/Common/DTOs/OrderDto.cs
@@ -0,0 +1,20 @@
+﻿namespace DainikBazar.Application.Common.DTOs;
+
+public class OrderDto
+{
+    public int Id { get; set; }
+    public string UserId { get; set; }
+    public decimal SubtotalPrice { get; set; }
+    public decimal DeliveryCharge { get; set; }
+    public decimal TotalPrice => SubtotalPrice + DeliveryCharge;
+    public int ProductId { get; set; }
+    public int Quantity { get; set; }
+    public int UnitPrice { get; set; }
+    public string OrderStatus { get; set; }
+    public DateTime OrderDate { get; set; }
+    public string PaymentMethod { get; set; }
+    public string OrderHistory { get; set; }
+    public string ReceiverAddress { get; set; }
+    public string ReceiverPhone { get; set; }
+    public string ReceiverName { get; set; }
+}
